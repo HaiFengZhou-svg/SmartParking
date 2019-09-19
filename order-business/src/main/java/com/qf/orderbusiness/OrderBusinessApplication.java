7e77c39d3e89d01b47e73ce17f03cfbe6c13d6a1
HaiFengZhou-svg <634888219@qq.com>
2019-09-19 15:06:16 +0800

    Initial commit

---
+++ order-business/src/main/java/com/qf/orderbusiness/OrderBusinessApplication.java
@@ -0,0 +1,13 @@
+package com.qf.orderbusiness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderBusinessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run( OrderBusinessApplication.class, args );
+    }
+
+}
---
+++ user-business/src/main/java/com/qf/userbusiness/UserBusinessApplication.java
@@ -0,0 +1,14 @@
+package com.qf.userbusiness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserBusinessApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run( UserBusinessApplication.class, args );
+    }
+
+}
